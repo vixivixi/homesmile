22e4c7c6b8ad9497e277f1ab1f19ca101b2969b1
vixi <vx@ice07.ru>
2019-05-04 14:23:20 +0300

    refactor move classes to lib

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?
+require_once('lib/main_class.php');
 $path = realpath('img/');
 $Directory = new RecursiveDirectoryIterator($path);
 // $filter = new MyRecursiveFilterIterator($Directory);
@@ -24,46 +25,6 @@ function pre($str){
 echo '<pre>';
   var_dump($str);
 echo '<pre>';
-}
-function OnlyDigits($str){
-  //https://stackoverflow.com/questions/33993461/php-remove-all-non-numeric-characters-from-a-string
-  $res = preg_replace("/[^0-9]/", "", $str );
-  return $res;
-}
-function bubblesort($arr){
-
-  $size = count($arr)-1;
-     for ($i=0; $i<$size; $i++) {
-         for ($j=0; $j<$size-$i; $j++) {
-             $k = $j+1;
-             if ($arr[$k]['width'] < $arr[$j]['width']) {
-             	$temp = $arr[$k];
-             	$arr[$k] = $arr[$j];
-             	$arr[$j] = $temp;
-             }
-         }
-     }
-return $arr;
-}
-
-class MyRecursiveFilterIterator extends \RecursiveFilterIterator {
-
-  public function accept() {
-    $filename = $this->current()->getFilename();
-    // Skip hidden files and directories.
-    if ($name[0] === '.') {
-      return FALSE;
-    }
-    if ($this->isDir()) {
-      // Only recurse into intended subdirectories.
-      return $name === 'wanted_dirname';
-    }
-    else {
-      // Only consume files of interest.
-      return strpos($name, 'wanted_filename') === 0;
-    }
-  }
-
 }
 
 
