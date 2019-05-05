434361d3503a8ce58e89ebc6f980f61bafa59d4e
vixi <vx@ice07.ru>
2019-05-06 01:36:41 +0300

    PRO-1015 Правки от наставника

--- src/index.php
+++ src/index.php
@@ -1,14 +1,27 @@
 <?php
+
 /**
- * @param  string sorting images via width size;
+ * This file index image dir and produce js file
+
+ *
+ * @category Array_Processing
+ * @package  Pagepackage
+ * @author   Display Name <vx@ice07.ru>
+ * @license  NoLicense
+ * @version  GIT: $Id$ In development. Very unstable.
+ * @link     NoLink
+ * 
  */
+
 require 'commands/main_class.php';
 $IMGDIR = 'img/';
 $JSONPATH = 'json/';
 
 $path = realpath($IMGDIR);
 $Directory = new RecursiveDirectoryIterator($path);
+
 // $filter = new MyRecursiveFilterIterator($Directory);
+
 $Iterator = new RecursiveIteratorIterator($Directory);
 $tool = new tool();
 
@@ -16,12 +29,14 @@ $files = array();
 $pre = array();
 foreach ($Iterator as $info) {
     if(!is_dir($info->getFilename())) {
-        $pre['name']=$info->getFilename();
-        $pre['path']=$info->getPathname();
-        $pre['size']= getimagesize($info->getPathname())[3] ;
-        $pre['width'] = tool::onlyDigits(explode(' ', getimagesize($info->getPathname())[3])[0]);
-        $pre['height'] = tool::onlyDigits(explode(' ', getimagesize($info->getPathname())[3])[1]);
-        $pre['type']= getimagesize($info->getPathname())['mime'] ;
+        $pre['name'] = $info->getFilename();
+        $pre['path'] = $info->getPathname();
+        $pre['size'] = getimagesize($info->getPathname())[3] ;
+        $width = explode(' ', getimagesize($info->getPathname())[3])[0];
+        $pre['width'] = Tool::onlyDigits($width);
+        $height = explode(' ', getimagesize($info->getPathname())[3])[1];
+        $pre['height'] = Tool::onlyDigits($height);
+        $pre['type'] = getimagesize($info->getPathname())['mime'] ;
         $files[] = $pre;
     }
 }
